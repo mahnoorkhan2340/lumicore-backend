f560989b331affd67e4346f7d02aba1ad696a864
Mahnoor <mahnoor1027@gmail.com>
2026-01-08 21:08:20 +0400

    api's done

--- cleaning/urls.py
+++ cleaning/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("normalize/", views.normalize_data, name="normalize_data"),
+    path("normalize", views.normalize_data, name="normalize_data"),
 ]
