1ca56f2f46b3b3279c7c3a4199d7cba49016d0cd
Mahnoor <mahnoor1027@gmail.com>
2026-01-09 23:19:44 +0400

    url issue resolved

--- cleaning/urls.py
+++ cleaning/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("data/", views.fetch_raw_data, name="fetch_raw_data"),
-    path("normalize/", views.normalize_data, name="normalize_data"),
-    path("submit/", views.submit_cleaned_data, name="submit_cleaned_data"),
+    path("data", views.fetch_raw_data, name="fetch_raw_data"),
+    path("normalize", views.normalize_data, name="normalize_data"),
+    path("submit", views.submit_cleaned_data, name="submit_cleaned_data"),
 ]
